7db240d6ed14d4fe7b061ae7e45515c1524dd32a
Joana Lima <joana.lima@liferay.com>
2022-12-01 15:55:51 -0300

    LPS-146312 Create Claims Settled Chart

--- modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/common/services/Claim.js
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/group/src/common/services/Claim.js
@@ -16,6 +16,40 @@ import {axios} from './liferay/api';
 
 const DeliveryAPI = 'o/c/raylifeclaims';
 
+export function getClaims() {
+	return axios.get(`${DeliveryAPI}/`);
+}
+
 export function getClaimsStatus(status) {
 	return axios.get(`${DeliveryAPI}/?filter=claimStatus eq '${status}'`);
+}
+
+export function getActiveClaims() {
+	return axios.get(`${DeliveryAPI}/?filter=claimStatus ne 'declined'`);
+}
+
+export function getClaimsByPeriod(
+	currentYear,
+	currentMonth,
+	currentDay,
+	periodYear,
+	periodMonth,
+	periodDay
+) {
+	return axios.get(
+		`${DeliveryAPI}/?fields=claimCreateDate,claimStatus,r_policyToClaims_c_raylifePolicyERC&pageSize=200&filter=claimStatus ne 'declined' and claimCreateDate le ${currentYear}-${currentMonth}-${currentDay} and claimCreateDate ge ${periodYear}-${periodMonth}-${periodDay}`
+	);
+}
+
+export function getSettledClaims(
+	currentYear,
+	currentMonth,
+	currentDay,
+	periodYear,
+	periodMonth,
+	periodDay
+) {
+	return axios.get(
+		`${DeliveryAPI}/?fields=claimCreateDate,claimStatus,r_policyToClaims_c_raylifePolicyERC&pageSize=200&filter=claimStatus eq 'settled' and claimCreateDate le ${currentYear}-${currentMonth}-${currentDay} and claimCreateDate ge ${periodYear}-${periodMonth}-${periodDay}`
+	);
 }
